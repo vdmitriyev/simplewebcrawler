832f30afa9554a48a16c4ab5b15cd379410f555b
Viktor Dmitriyev <dmitriyev.viktor@gmail.com>
2015-02-10 00:06:21 +0100

    Adding dynamic method of parsing. Example parses website with weather data for the two cities in Russia.

--- simplewebcrawler/crawler_main_bs.py
+++ simplewebcrawler/crawler_main_bs.py
@@ -12,21 +12,23 @@ __status__     = "Test"
 __date__    = "23.01.2015"
 __description__ = "Simple web crawler that uses beautifulsoup package."
 
-
+import urllib2
 from time import sleep
 from crawler_helper import DirectoryHelper
-import urllib2
 from bs4 import BeautifulSoup
+import collections
 from pprint import pprint
+import uuid
+import json
 
-# SLEEP_TIME_IN_SECONDS = 1
+SLEEP_TIME_IN_SECONDS = 2
 class BSCrawler():
 
     UA = 'Mozilla/5.0 (X11; U; FreeBSD i386; en-US; rv:1.9.2.9) Gecko/20100913 Firefox/3.6.9'
 
     def __init__(self):
         self.helper = DirectoryHelper()
-        self.helper.prepare_working_directory()
+        #self.helper.prepare_working_directory()
         self.temp_dir = self.helper.temp_dir
 
     def crawl(self):
@@ -42,19 +44,34 @@ class BSCrawler():
             doc = self.download_document(url)
             self.dispatcher[url](self, doc)
 
+    def crawl_dynamic(self):
+        """
+            (obj) -> None
+
+            Method that extracts urls from 'dispatcher' in a dynamic way.
+            It means that the initial URL patter should contain '%s' string patter to be formatted.
+        """
+
+        for url in self.dispatcher_dynamic:
+            print '[i] following url is going to be parsed in a dynamic way:\n %s '% (url)
+            self.dynamic_url = url
+            tag = self.dispatcher_dynamic[url][1]
+            self.dispatcher_dynamic[url][0](self, tag)
+
     def download_document(self, url):
         """
             (obj,str) -> (str)
 
             Downloading html page and storing inside string.
         """
+
         html = None
         try:
             req = urllib2.Request(url=url, headers={'User-Agent': self.UA})
             hdl = urllib2.urlopen(req)
             html = hdl.read()
         except Exception,ex:
-            print '[e] Exception: %s ' % str(ex)
+            print '[e] exception: {}'.format(str(ex))
 
         return html
 
@@ -172,14 +189,121 @@ class BSCrawler():
         self.helper.save_file(self.temp_dir + gen_new_name + '.output', output)
 
 
+    def process_tutiempo_weather(self, tag):
+        """
+            (obj) -> None
+
+            Processing weather data of the city from the tutiempo.
+
+            KRASNOYARSK: http://en.tutiempo.net/climate/ws-284935.html
+            NOVOSIBIRSK: http://en.tutiempo.net/climate/ws-296340.html
+        """
+
+        delimeter = ';'
+
+        def internal_parser(html):
+            """
+                (str) -> dict
+
+                Internal parser that parses given html accoding to it's structure.
+            """
+
+            soup = BeautifulSoup(html)
+            data = []
+            table = soup.find('table', attrs={'class':'medias mensuales'})
+            rows = table.findAll('tr')
+            for row in rows:
+                cols = row.findAll('td')
+                cols = [ele.text.strip() for ele in cols]
+                data.append([ele for ele in cols if ele]) # Get rid of empty values
+
+            daily_weather = {}
+            for row in data:
+                if len(row) > 3:
+                    try:
+                        day = int(row[0])
+                        if day >= 1 and day <= 31:
+                            daily_weather[day] = [row[2], row[3]]
+                    except Exception, ex:
+                        pass
+                        #print '[e] exception: {}'.format(str(ex))
+
+            # sorting values to have them in order
+            daily_weather = collections.OrderedDict(sorted(daily_weather.items()))
+            return daily_weather
+
+        #params for testing
+        years = ['1999']
+        months = ['01']
+
+        #params for real processing
+        #years = ['1999', '2000', '2001', '2002']
+        #months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+        try:
+            if years is None or len(years) < 1:
+                raise Exception('years should be defined')
+            if months is None or len(months) < 1:
+                raise Exception('months should be defined')
+        except Exception, ex:
+            print '[e] exception: {}'.format(str(ex))
+            return
+
+        result_csv = list()
+        for year in years:
+            for month in months:
+                month_year = '{}-{}'.format(month,year)
+                url_to_download = self.dynamic_url % (month_year)
+
+                # priting info about url to download
+                print '[i] url to download {}'.format(url_to_download)
+
+                month_weather = internal_parser(self.download_document(url_to_download))
+                for key in month_weather:
+                    tmp_time = '{}-{}-{}'.format(str(year), str(month), str(key))
+                    tmp_max = str(month_weather[key][0])
+                    tmp_min = str(month_weather[key][1])
+                    result_csv.append([tmp_time, tmp_max, tmp_min])
+
+                # setting the main script to sleep for some seconds
+                sleep(SLEEP_TIME_IN_SECONDS)
+
+        # generating new file name
+        _new_file_name = str(uuid.uuid1())
+
+        # converting to the csv
+        csv_output = ''
+
+        # setting header of the csv
+        prefix = tag[0:3]
+        csv_output =  prefix + 'Time' + delimeter + prefix + 'MaxTemp' + delimeter + prefix + 'MinTemp' + '\n'
+
+        for row in result_csv:
+            tmp = ''
+            for value in row:
+                tmp += value + delimeter
+            csv_output += tmp[:-1*len(delimeter)] + '\n'
+
+        # saving csv output to in the file
+        csv_full_path = self.temp_dir + tag + '-' + _new_file_name + '.csv'
+        self.helper.save_file(csv_full_path, csv_output)
+        print '[i] data in csv saved to the {}'.format(csv_full_path)
+
+    # dispatchers for extract and parsing one single web page
     dispatcher = {
-            'http://diglib.bis.uni-oldenburg.de/hsb/statistik/?page=hsb_institut&jahr=2012&inst=20100': process_biblio_oldb
+            'http://diglib.bis.uni-oldenburg.de/hsb/statistik/?page=hsb_institut&jahr=2012&inst=20100': process_biblio_oldb,
+        }
+
+    # dispatchers for extract and parsing multiple web pages
+    dispatcher_dynamic = {
+            'http://en.tutiempo.net/climate/%s/ws-284935.html': [process_tutiempo_weather, "KRASNOYARSK"],
+            'http://en.tutiempo.net/climate/%s/ws-296340.html': [process_tutiempo_weather, "NOVOSIBIRSK"],
         }
 
 def main():
 
     crawler = BSCrawler()
-    crawler.crawl()
+    #crawler.crawl()
+    crawler.crawl_dynamic()
 
 if __name__ == '__main__':
     main()
